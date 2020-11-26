257f3ca9e463503bd49177ea0bbead14b3218c11
Tarek Arif <tarifwell@gmail.com>
2020-11-26 17:30:20 +0100

    update checkpoint 33

---
+++ Tarek_Arif_Chkpt_33_SQL-DML(customer-database)/SQL-DML-database/CUSTOMER_DATABASE - MySQL_Export_project.sql
@@ -0,0 +1,125 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : jeu. 26 nov. 2020 à 16:59
+-- Version du serveur :  10.4.11-MariaDB
+-- Version de PHP : 7.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `CUSTOMER_DATABASE`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `customer`
+--
+
+CREATE TABLE `customer` (
+  `Customer_id` varchar(20) NOT NULL,
+  `customer_Name` varchar(20) NOT NULL,
+  `customer_Tel` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `customer`
+--
+
+INSERT INTO `customer` (`Customer_id`, `customer_Name`, `customer_Tel`) VALUES
+('C01', 'ALI', 71321009),
+('C02', 'ASMA', 77345823);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `orders`
+--
+
+CREATE TABLE `orders` (
+  `Customer_id` varchar(20) NOT NULL,
+  `Product_id` varchar(20) NOT NULL,
+  `quantity` int(11) DEFAULT 0,
+  `total_amount` int(11) DEFAULT NULL,
+  `OrderDate` date DEFAULT sysdate()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `orders`
+--
+
+INSERT INTO `orders` (`Customer_id`, `Product_id`, `quantity`, `total_amount`, `OrderDate`) VALUES
+('C01', 'P02', 2, 9198, NULL),
+('C02', 'P01', 1, 3299, '2020-05-28');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `product`
+--
+
+CREATE TABLE `product` (
+  `Product_id` varchar(20) NOT NULL,
+  `product_name` varchar(20) NOT NULL,
+  `Price` int(11) DEFAULT NULL CHECK (`Price` > 0),
+  `Category` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `product`
+--
+
+INSERT INTO `product` (`Product_id`, `product_name`, `Price`, `Category`) VALUES
+('P01', 'Samsung Galaxy S20', 3299, 'Smartphone'),
+('P02', 'ASUS Notebook', 4599, 'PC');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`Customer_id`);
+
+--
+-- Index pour la table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`Customer_id`,`Product_id`),
+  ADD KEY `Product_id` (`Product_id`);
+
+--
+-- Index pour la table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`Product_id`);
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`Customer_id`) REFERENCES `customer` (`Customer_id`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`Product_id`) REFERENCES `product` (`Product_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
